94deeed7bad872004d0793db861762ed1682436f
anfebuba <andresfelipebulabaines@gmail.com>
2020-01-11 21:14:47 -0500

    Function to print numbers between a given range

--- funciones.py
+++ funciones.py
@@ -1,2 +1,10 @@
-for i in range(2,8,3):
-    print(i)
+def numerosentre(a,b):
+    for i in range(a+1,b,1):
+        print(i)
+
+n1=int(input('escriba el primero de dos números para mostrar los numeros que hay entre uno y otro: '))
+n2= int(input('escriba el segundo de dos números para mostrar los numeros que hay entre uno y otro: '))
+
+
+print('los numeros son: '), numerosentre(n1,n2)
+
